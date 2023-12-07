96761195bbe49308c53f0f9be0ab877d94f5ccb7
Antonio Perez <antonio.perez@gametime.co>
2023-12-07 17:04:13 -0600

    Solve second part of the challange

--- main.go
+++ main.go
@@ -13,46 +13,31 @@ import (
 type Hand struct {
     Rank  int
     Cards string
-    BiD   int
+    Bid   int
 }
 
-const (
-    handLen = 5
-)
-
 var (
-    labelValues = map[rune]int{
-        '2': 0,
-        '3': 1,
-        '4': 2,
-        '5': 3,
-        '6': 4,
-        '7': 5,
-        '8': 6,
-        '9': 7,
-        'T': 8,
-        'Q': 9,
-        'K': 10,
-        'A': 11,
+    labelsMap = map[rune]rune{
+        '2': 'A',
+        '3': 'B',
+        '4': 'C',
+        '5': 'D',
+        '6': 'E',
+        '7': 'F',
+        '8': 'G',
+        '9': 'H',
+        'T': 'I',
+        'J': 'J',
+        'Q': 'K',
+        'K': 'L',
+        'A': 'M',
     }
 )
 
 type ByHandCards []Hand
 
-func (hs ByHandCards) Len() int      { return len(hs) }
-func (hs ByHandCards) Swap(i, j int) { hs[i], hs[j] = hs[j], hs[i] }
-func (hs ByHandCards) Less(i, j int) bool {
-    for c := 0; c < handLen; c++ {
-        if labelValues[rune(hs[i].Cards[c])] < labelValues[rune(hs[j].Cards[c])] &&
-            hs[i].Rank <= hs[j].Rank {
-            return true
-        }
-    }
-    return false
-}
-
 func main() {
-    pf, err := os.Open("example.txt")
+    pf, err := os.Open("input.txt")
     if err != nil {
         log.Fatalf("while opening file %q: %s", pf.Name(), err)
     }
@@ -64,10 +49,11 @@ func main() {
 
     for scnr.Scan() {
         fields := strings.Split(scnr.Text(), " ")
+        Cards := strings.Map(func(r rune) rune { return labelsMap[r] }, strings.TrimSpace(fields[0]))
         Bid, _ := strconv.Atoi(strings.TrimSpace(fields[1]))
         hand := Hand{
-            Cards: strings.TrimSpace(fields[0]),
-            BiD:   Bid,
+            Cards: Cards,
+            Bid:   Bid,
         }
         switch {
         case hc(hand.Cards):
@@ -89,19 +75,16 @@ func main() {
         hands = append(hands, hand)
     }
 
-    sort.Sort(hands)
-
-    sort.Slice(hands, func(i, j int) bool {
+    sort.SliceStable(hands, func(i, j int) bool {
         return hands[i].Rank < hands[j].Rank
     })
-
-    for i := 0; i < len(hands); i++ {
-        hands[i].Rank = i + 1
-    }
+    sort.SliceStable(hands, func(i, j int) bool {
+        return hands[i].Cards < hands[j].Cards && hands[i].Rank == hands[j].Rank
+    })
 
     result := int64(0)
     for i := 0; i < len(hands); i++ {
-        result += int64(hands[i].Rank * hands[i].BiD)
+        result += int64((i + 1) * hands[i].Bid)
     }
 
     for _, hand := range hands {
@@ -112,99 +95,88 @@ func main() {
 }
 
 func kao5(hand string) bool {
-    label := hand[0]
-    for _, card := range hand {
-        if card != rune(label) {
-            return false
+    max := 0
+    labels := make(map[byte]int)
+    for i := 0; i < len(hand); i++ {
+        labels[hand[i]]++
+        if max < labels[hand[i]] {
+            max = labels[hand[i]]
         }
     }
 
-    return true
+    return len(labels) == 1 && max == 5
 }
 
 func kao4(hand string) bool {
-    swaps := 0
-    label := hand[0]
-    for _, card := range hand {
-        if card != rune(label) {
-            swaps++
+    max := 0
+    labels := make(map[rune]int)
+    for _, label := range hand {
+        labels[label]++
+        if max < labels[label] {
+            max = labels[label]
         }
     }
 
-    if swaps > 2 {
-        return false
-    }
-
-    return true
+    return len(labels) == 2 && max == 4
 }
 
 func fh(hand string) bool {
-    swaps := 0
-    label := hand[0]
-    for _, card := range hand {
-        if card != rune(label) {
-            swaps++
+    max := 0
+    labels := make(map[rune]int)
+    for _, label := range hand {
+        labels[label]++
+        if max < labels[label] {
+            max = labels[label]
         }
     }
 
-    if swaps > 3 {
-        return false
-    }
-
-    return true
+    return len(labels) == 2 && max == 3
 }
 
 func kao3(hand string) bool {
-    cards := make(map[rune]int)
-    for _, card := range hand {
-        cards[card]++
-    }
-
-    for _, value := range cards {
-        if value == 3 {
-            return true
+    max := 0
+    labels := make(map[rune]int)
+    for _, label := range hand {
+        labels[label]++
+        if max < labels[label] {
+            max = labels[label]
         }
     }
 
-    return false
+    return len(labels) == 3 && max == 3
 }
 
 func pair2(hand string) bool {
-    pairs := 0
-    cards := make(map[rune]int)
-    for _, card := range hand {
-        cards[card]++
-    }
-
-    for _, value := range cards {
-        if value == 2 {
-            pairs++
+    max := 0
+    labels := make(map[rune]int)
+    for _, label := range hand {
+        labels[label]++
+        if max < labels[label] {
+            max = labels[label]
         }
     }
 
-    return pairs == 2
+    return len(labels) == 3 && max == 2
 }
 
 func pair(hand string) bool {
-    pairs := 0
-    cards := make(map[rune]int)
-    for _, card := range hand {
-        cards[card]++
-    }
-
-    for _, value := range cards {
-        if value == 2 {
-            pairs++
+    max := 0
+    labels := make(map[rune]int)
+    for _, label := range hand {
+        labels[label]++
+        if max < labels[label] {
+            max = labels[label]
         }
     }
 
-    return pairs == 1
+    return len(labels) == 4 && max == 2
 }
 
 func hc(hand string) bool {
-    if hand[0] != hand[1] && hand[1] != hand[2] &&
-        hand[2] != hand[3] && hand[3] != hand[4] {
-        return true
+    labels := make(map[rune]int)
+    for _, label := range hand {
+        labels[label]++
     }
-    return false
+
+    return len(labels) == 5
 }
